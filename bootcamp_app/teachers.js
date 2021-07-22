eb31fe594d589cf8e8cf04ade6e661d558485121
fictionalparakeets <banksmatthew32@gmail.com>
2021-07-22 00:20:20 +0000

    update with parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -14,15 +14,21 @@ pool.connect().then(() => {
 })
 
 
-pool.query(`
+const cohortsName = process.argv[2] || 'JUL02';
+// Store all potentially malicious values in an array.
+const values = [`%${cohortsName}%`];
+
+
+pool
+.query(`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM assistance_requests
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 JOIN teachers ON teacher_id = teachers.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(entry => {
     console.log(`${entry.cohort}: ${entry.teacher}`);
